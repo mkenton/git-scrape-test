a1fb7de06209a5e75a30f238e464da3905de693e
Michael <mkenton2020@icloud.com>
2021-09-14 02:20:26 -0500

    scrape for no. commits, datetime, and basic date format

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,24 @@
+require 'pry'
 require 'nokogiri'
 require 'open-uri'
 
-html = open("https://flatironschool.com/")
+user = "mkenton"
+labs = ["phase-3-ruby-oo-self-count-sentences-lab"]
+url = "https://github.com/#{user}/#{labs[0]}/tree/fis-wip"
+html = URI.open(url)
+
+doc = Nokogiri::HTML(html)
+@num_commits = doc.search('//li//a//span//strong').text
+
+@commit = doc.search('//relative-time')
+@timedate_of_commit = @commit.first.attributes['datetime']
+@date_of_commit = @commit.first.text
+
+puts "number of commits: #{@num_commits}"
+puts
+puts "date/time of commit: #{@timedate_of_commit}"
+puts
+puts "Date of commit: #{@date_of_commit}"
+# binding.pry
+
+
